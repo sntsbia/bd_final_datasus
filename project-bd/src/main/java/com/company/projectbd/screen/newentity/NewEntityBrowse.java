5799b674c7156fa27f91f39edad84e01778c4066
sntsb <b.santos@intelipec.com.br>
2022-08-03 23:20:01 -0300

    Arrumando modelo Fisico e queries

--- project-bd/src/main/java/com/company/projectbd/screen/newentity/NewEntityBrowse.java
+++ project-bd/src/main/java/com/company/projectbd/screen/newentity/NewEntityBrowse.java
@@ -6,7 +6,6 @@ import io.jmix.ui.screen.*;
 import com.company.projectbd.dto.PessoasFebre;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Collections;
 import java.util.List;
 
 @UiController("NewEntity.browse")
@@ -14,11 +13,11 @@ import java.util.List;
 @LookupComponent("newEntitiesTable")
 public class NewEntityBrowse extends StandardLookup<PessoasFebre> {
     @Autowired
-    private NewEntityService pessoasFebre;
+    private NewEntityService pessoasFebreService;
 
     @Install(to = "newEntitiesDl", target = Target.DATA_LOADER)
     private List<PessoasFebre> newEntitiesDlLoadDelegate(LoadContext<PessoasFebre> loadContext) {
         // Here you can load entities from an external store
-        return pessoasFebre.getPessoasFebre();
+        return pessoasFebreService.getPessoasFebre();
     }
 }
